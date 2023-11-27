1268f90bcae4fefaf4b05fbdfb004d2a3dce7386
jijiezhou <jijiez@email.unc.edu>
2023-11-26 19:15:42 -0500

    add rpsls.js

--- public/scripts.js
+++ public/scripts.js
@@ -2,10 +2,6 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-// If you would like to see some examples of similar code to make an interface interact with an API,
-// check out the coin-server example from a previous COMP 426 semester.
-// https://github.com/jdmar3/coinserver
-
 function play() {
   const rps = document.getElementById("rps");
   const rpsls = document.getElementById("rpsls");
